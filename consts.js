34553646ac23e1f7e5173811b1e50abae2e85729
Sam Peterson <samuel.e.peterson@gmail.com>
2019-08-15 23:28:31 -0500

    finalize initial tool

--- consts.js
+++ consts.js
@@ -2,6 +2,10 @@ const NO_ARGS_ERROR         = 'No directory argument was received to register'
 const TOO_MANY_ARGS_ERROR   = 'Only one directory argument was expected, instead received:'
 const COMMAS_INCLUDED_ERROR = 'Commas cannot be included in the name of a path alias'
 
+const PACKAGE_CONFIG_KEY = '_moduleAliases'
+const TS_CONFIG_KEY      = 'paths'
+
+const NEW_LINE_WITH_1_TAB  = '\n    '
 const NEW_LINE_WITH_2_TABS = '\n        '
 const NEW_LINE_WITH_3_TABS = '\n            '
 
@@ -9,6 +13,9 @@ module.exports = {
     NO_ARGS_ERROR,
     TOO_MANY_ARGS_ERROR,
     COMMAS_INCLUDED_ERROR,
+    NEW_LINE_WITH_1_TAB,
     NEW_LINE_WITH_2_TABS,
-    NEW_LINE_WITH_3_TABS
+    NEW_LINE_WITH_3_TABS,
+    PACKAGE_CONFIG_KEY,
+    TS_CONFIG_KEY
 }
--- main.js
+++ main.js
@@ -2,15 +2,21 @@ const fs = require('fs')
 
 const { isArrayWithValue } = require('./utils')
 
-const { NEW_LINE_WITH_2_TABS, NEW_LINE_WITH_3_TABS } = require('./consts')
+const {
+    NEW_LINE_WITH_1_TAB,
+    NEW_LINE_WITH_2_TABS,
+    NEW_LINE_WITH_3_TABS,
+    PACKAGE_CONFIG_KEY,
+    TS_CONFIG_KEY
+} = require('./consts')
 
-const updateTsPaths = (applicationRoot, newAlias) => {
-    const filePath = `${applicationRoot}/tsconfig.json`
-
+const updateConfig = ({ filePath, newConfigText, configKey, shortIndent, longIndent }) => {
     const file = fs.readFileSync(filePath, 'utf-8')
 
-    const [prePathText, postPathText] = file.split('"paths": {')
-    const [configText, postConfigText] = postPathText.split('},')
+    const [prePathText, postPathText]      = file.split(`"${configKey}": {`)
+    const configDivider                    = postPathText.includes('},') ? '},' : '}'
+    const [configText, ...postConfigTexts] = postPathText.split(configDivider)
+    const postConfigText                   = postConfigTexts.join(configDivider)
 
     let cleanedConfigText = configText.trim()
 
@@ -20,22 +26,58 @@ const updateTsPaths = (applicationRoot, newAlias) => {
         [] :
         existingConfigs.map(config => config.trim())
 
-    const newConfigText = `"${newAlias}": ["${newAlias}"]`
-
     const updatedConfigs = cleanedExistingConfigs.length === 0 ?
         [newConfigText] :
         [...cleanedExistingConfigs, newConfigText]
 
-    let updatedConfigString = updatedConfigs.join(`${updatedConfigs.length === 1 ? '' : ','}${NEW_LINE_WITH_3_TABS}`)
-    updatedConfigString = `${NEW_LINE_WITH_3_TABS}${updatedConfigString}${NEW_LINE_WITH_2_TABS}`
+    let updatedConfigString = updatedConfigs.join(`${updatedConfigs.length === 1 ? '' : ','}${longIndent}`)
+    updatedConfigString = `${longIndent}${updatedConfigString}${shortIndent}`
 
-    const finalFile = prePathText + '"paths": {' + updatedConfigString + '},' + postConfigText
+    const finalFile = prePathText + `"${configKey}": {` + updatedConfigString + configDivider + postConfigText
 
-    console.log(finalFile)
+    fs.writeFileSync(filePath, finalFile)
+}
+
+const updatePackagePaths = (applicationRoot, newAlias, newAliasMapping) => {
+    const [updatedAliasText, updatedAliasMapping] = [newAlias, newAliasMapping].map(aliasType =>
+        aliasType.slice(aliasType.length - 2) === '/*' ?
+            aliasType.slice(0, aliasType.length - 2) :
+            aliasType
+    )
+
+    const filePath      = `${applicationRoot}/package.json`
+    const newConfigText = `"${updatedAliasText}": "dist/src/${updatedAliasMapping}"`
+
+    updateConfig({
+        filePath,
+        newConfigText,
+        configKey: PACKAGE_CONFIG_KEY,
+        shortIndent: NEW_LINE_WITH_1_TAB,
+        longIndent: NEW_LINE_WITH_2_TABS
+    })
+}
+
+const updateTsPaths = (applicationRoot, newAlias, newAliasMapping) => {
+    const filePath      = `${applicationRoot}/tsconfig.json`
+    const newConfigText = `"${newAlias}": ["${newAliasMapping}"]`
+
+    updateConfig({
+        filePath,
+        newConfigText,
+        configKey: TS_CONFIG_KEY,
+        shortIndent: NEW_LINE_WITH_2_TABS,
+        longIndent: NEW_LINE_WITH_3_TABS
+    })
 }
 
 function injectImportAlias(applicationRoot, newAlias) {
-    updateTsPaths(applicationRoot, newAlias)
+    const aliasMapping = newAlias[0].match(/[^a-zA-Z0-9]/) ?
+        newAlias.slice(1, newAlias.length) :
+        newAlias
+
+    updateTsPaths(applicationRoot, newAlias, aliasMapping)
+
+    updatePackagePaths(applicationRoot, newAlias, aliasMapping)
 }
 
 module.exports = injectImportAlias
